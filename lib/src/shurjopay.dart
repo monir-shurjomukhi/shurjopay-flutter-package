f4ff3d645b77c2ef258c8d4ea04fd29787b43431
Monir <mknishad1@ymail.com>
2021-12-06 18:58:00 +0600

    add shurjopay widget

--- lib/src/shurjopay.dart
+++ lib/src/shurjopay.dart
@@ -1,2 +1,28 @@
 import 'package:flutter/material.dart';
 
+class Shurjopay extends StatefulWidget {
+  const Shurjopay({
+    Key? key,
+    this.amount
+  }) : super(key: key);
+
+  final double? amount;
+
+  @override
+  _ShurjopayState createState() => _ShurjopayState();
+}
+
+class _ShurjopayState extends State<Shurjopay> {
+
+  @override
+  void initState() {
+    super.initState();
+
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
